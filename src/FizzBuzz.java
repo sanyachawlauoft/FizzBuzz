266f14d7c03867716a17b0debcf7b8c1478be95f
Sanya Chawla <sanya@Sanyas-MacBook-Pro.local>
2023-09-11 19:20:27 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -30,5 +30,39 @@ class FizzBuzz {
 
             }
         }
+
+        int count = 1;
+        while (count < 100) {
+
+            count = doFizzBuzz(count);
+        }
+    }
+
+    private static int doFizzBuzz(int count) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = count % 3 == 0;
+        boolean divisibleBy5 = count % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(count);
+
+        }
+
+        count++;
+        return count;
     }
 }
